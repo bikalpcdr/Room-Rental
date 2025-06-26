3550295ee431c291b38d6187e884624e4f66f4cd
bikalpcdr <bikalpcdr42@gmail.com>
2025-06-26 23:07:12 +0545

    feat: security and auth related things and role base access

--- src/main/java/com/bikalp/roomrentalservice/constant/AppConstant.java
+++ src/main/java/com/bikalp/roomrentalservice/constant/AppConstant.java
@@ -3,4 +3,4 @@ package com.bikalp.roomrentalservice.constant;
 public class AppConstant {
     public static final String SECRET_KEY = "bikalp-secret-key";
     public static final Integer TOKEN_EXPIRY_DURATION = 1000 * 60 * 60 * 10;
-} 
+}
