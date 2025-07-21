bbd362e0cc3bd5840fa0c652e63fcef129846b1c
bikalp-java <bikalpa.chaudhary@yenyasoft.com>
2025-07-21 15:14:57 +0545

    fix: fix images uploading issue with property.

--- src/main/java/com/bikalp/roomrentalservice/controller/PropertyController.java
+++ src/main/java/com/bikalp/roomrentalservice/controller/PropertyController.java
@@ -9,10 +9,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
