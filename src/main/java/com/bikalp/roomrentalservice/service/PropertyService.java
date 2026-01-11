7c231cc8fba754bb6a477ed0d5d185bb6db47583
bikalp-java <bikalpa.chaudhary@yenyasoft.com>
2026-01-11 19:25:15 +0545

    fix: support langitude and latitude in both back and front

--- src/main/java/com/bikalp/roomrentalservice/service/PropertyService.java
+++ src/main/java/com/bikalp/roomrentalservice/service/PropertyService.java
@@ -11,6 +11,8 @@ import java.util.List;
 public interface PropertyService {
     void createProperty(PropertyRequest request);
 
+    Long createPropertyWithImages(PropertyRequest request, List<MultipartFile> images);
+
     void updateProperty(PropertyRequest request);
 
     void deleteProperty(Long propertyId);
@@ -28,4 +30,4 @@ public interface PropertyService {
     List<PropertyResponse> searchProperty(FilterRequest request);
 
     List<BookingResponse> fetchBookingRequest();
-} 
+}
