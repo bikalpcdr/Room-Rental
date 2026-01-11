809ac1cd391e16b7e37a2e685847505a89cd63a9
bikalp-java <bikalpa.chaudhary@yenyasoft.com>
2026-01-11 08:41:45 +0545

    Revert "intial auditing fix"
    This reverts commit 3b3ed66d48cd194f090409157eae15b2cffb77bb.

--- src/main/java/com/bikalp/roomrentalservice/config/AuditorConfig.java
+++ src/main/java/com/bikalp/roomrentalservice/config/AuditorConfig.java
@@ -16,13 +16,7 @@ public class AuditorConfig {
     private final UserDataConfig userDataConfig;
 
     @Bean
-    public AuditorAware<Long> auditorProvider() {
-        return () -> {
-            try {
-                return Optional.ofNullable(userDataConfig.getCurrentUserId());
-            } catch (Exception e) {
-                return Optional.of(1L);
-            }
-        };
+    public AuditorAware<Integer> auditorProvider() {
+        return () -> Optional.ofNullable(userDataConfig.getCurrentUserId().intValue());
     }
 }
